a7834a5c3fd2deab429deb1e553db4474656523b
HenrikObsen <hto@djes.dk>
2015-10-05 13:38:36 +0200

    Tilføjet DDMF

---
+++ WebItGrenaa/App_Code/MailFac.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+/// <summary>
+/// 
+//Brug af skolen SMTP
+//MailFac mf = new MailFac("vid233.vid.net.local", "hto@videndjurs.dk", "Henrik Obsen");
+
+//Brug din egen gmail og Googles SMTP
+//MailFac mf = new MailFac("smtp.gmail.com", "email@gmail.com", "DitNavne", "password", 587);
+
+//mf.Send("Emne","Body teksten.","hto@djes.dk");
+/// </summary>
+public class MailFac
+{
+    private string _SMTP = "";
+    private string _fromEmail = "";
+    private string _fromName = "";
+    private string _password = "";
+    private int _port = 25;
+
+
+	public MailFac(string SMTP, string fromEmail, string fromName)
+	{
+        _SMTP = SMTP;
+	    _fromEmail = fromEmail;
+	    _fromName = fromName;
+	}
+
+    public MailFac(string SMTP, string fromEmail, string fromName, string password, int port)
+    {
+        _SMTP = SMTP;
+        _fromEmail = fromEmail;
+        _fromName = fromName;
+        _password = password;
+        _port = port;
+    }
+
+    public void Send(string Subject, string Text, string Receiver)
+    {
+        MailMessage mail = new MailMessage();
+
+        mail.From = new MailAddress(_fromEmail, _fromName);
+
+        mail.To.Add(Receiver);
+        mail.Subject = Subject;
+        mail.Body = Text;
+        mail.IsBodyHtml = true;
+        
+        SmtpClient smtpCl = new SmtpClient(_SMTP, _port);
+
+        if (_password != "")
+        {
+        smtpCl.EnableSsl = true;
+        smtpCl.UseDefaultCredentials = false;
+        smtpCl.Credentials = new NetworkCredential(_fromEmail, _password);
+        smtpCl.DeliveryMethod = SmtpDeliveryMethod.Network;  
+        }
+       
+        smtpCl.Send(mail); 
+        
+    }
+    
+
+
+
+
+}
---
+++ WebItGrenaa/App_Code/ToolBox.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ToolBox
+/// </summary>
+public class ToolBox
+{
+    string strKey = "a b c d e f g h i j k l m n o p q r s t u v x y z 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V X Y Z";
+    string strKeyRev = "";
+    string newText = "";
+    string[] arrKey = { };
+
+	public ToolBox()
+	{
+	    foreach (char c in strKey)
+	    {
+	        strKeyRev = c + strKeyRev;
+	    }
+	}
+
+    
+
+    public string Krypter(string text)
+    {
+        arrKey = strKeyRev.Split(' ');
+      
+        foreach (char ch in text)
+        {
+            int plac = strKey.Replace(" ", "").IndexOf(ch);
+            
+            if (plac >= 0)
+            {
+                newText += arrKey[plac];
+            }
+            else
+            {
+                newText += ch;
+            }
+        }
+
+        return newText;
+    }
+
+
+    public string DeKrypter(string text)
+    {
+        arrKey = strKey.Split(' ');
+
+        foreach (char ch in text)
+        {
+            int plac = strKeyRev.Replace(" ", "").IndexOf(ch);
+
+            if (plac >= 0)
+            {
+                newText += arrKey[plac];
+            }
+            else
+            {
+                newText += ch;
+            }
+        }
+
+        return newText;
+    }
+}
